5fdffa240e0fbeb046aea0fe4ec64517ae465ef6
rahul chauham <rahulchauham0@gmail.com>
2024-07-07 08:35:28 +0000

    created a stage_orders model in stage folder. This will help to create aggregates models in reporting folder.

---
+++ models/STAGE/stage_orders.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice,
+    o.ordercostprice,
+    o.ordersellingprice - o.ordercostprice as total_profit,
+    c.customer_id,
+    c.customer_name,
+    c.segment,
+    c.country,
+    c.state,
+    p.category,
+    p.product_id,
+    p.product_name,
+    p.sub_category
+from {{ ref("raw_orders") }} as o
+left join {{ ref("raw_customers") }} as c on o.customerid = c.customer_id
+left join {{ ref("raw_products") }} as p on o.productid = p.product_id
